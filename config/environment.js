bf9073d24bffe9396bc69f5474c67819e7110e0e
Masa Takeuchi <masahitoart@gmail.com>
2021-02-12 13:45:26 +0000

    preparing deployement

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,14 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/pokezon'
-export const secret = 'this string is a special string'
+// export const port = 4000
+// export const dbURI = 'mongodb://localhost/pokezon'
+// export const secret = 'this string is a special string'
+
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/pokezon'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'
+
 
 
